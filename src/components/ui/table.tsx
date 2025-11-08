27ce680e0c7128828a0aaad9f2834de634a1bfa5
Dave Puchyr <dave@avaritia.com>
2025-11-08 04:00:58 +0000

    Make the height of the rows more compact.

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -41,10 +41,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<
    ({ className, ...props }, ref) => (
       <th
          ref={ref}
-         className={cn(
-            "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
-            className,
-         )}
+         className={cn("h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0", className)}
          {...props}
       />
    ),
@@ -53,7 +50,7 @@ TableHead.displayName = "TableHead";
 
 const TableCell = React.forwardRef<HTMLTableCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
    ({ className, ...props }, ref) => (
-      <td ref={ref} className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)} {...props} />
+      <td ref={ref} className={cn("p-2 align-middle [&:has([role=checkbox])]:pr-0", className)} {...props} />
    ),
 );
 TableCell.displayName = "TableCell";
